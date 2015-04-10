4cb7ae57bcca375c12a40c01c0d11d9eaad04e46
Roscoe <heiwu805@hotmail.com>
2015-04-10 19:03:47 +0800

    style update

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var helpChooseApp = angular.module("helpChooseApp",[
     'ngRoute','ngTouch','ngStorage','hcControllers'
 ]);
 
-var rootPath = 'http://' + location.hostname + '/helpmechoose/partials/'
+var rootPath = 'http://' + location.hostname + '/partials/'
 
 helpChooseApp.config(function($routeProvider){
     $routeProvider.when('/list',{
